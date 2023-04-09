23935030d9fc0e5c17cdfbae0b25a9823ac317b3
Kulpot <kulpotwahu@gmail.com>
2023-04-09 22:50:18 +0800

    Add project files.

---
+++ WinFormPropertyBindings/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormPropertyBindings
+{
+    class Class1 : INotifyPropertyChanged
+    {
+        private string _myStr;
+
+        public string MyStr 
+        {
+            get { return _myStr; } 
+            set 
+            {
+                //if(_myStr != value)
+                if (value == MyStr) return;
+                _myStr = value;
+                OnPropertyChanged("MyStr");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ WinFormPropertyBindings/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormPropertyBindings
+{
+    public partial class Form1 : Form
+    {
+        private Class1 _class1 = new Class1();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Binding binding = new Binding("Text", textBox2, "Text", 
+                true, DataSourceUpdateMode.OnPropertyChanged);
+
+
+            textBox1.DataBindings.Add(binding);
+
+            this.DataBindings.Add("Size", textBox3, "Text",
+                true, DataSourceUpdateMode.OnPropertyChanged);
+
+            textBox4.DataBindings.Add("Size", _class1 , "MyStr",
+                true, DataSourceUpdateMode.OnPropertyChanged);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _class1.MyStr = "Hey";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Text = _class1.MyStr;
+        }
+    }
+}
